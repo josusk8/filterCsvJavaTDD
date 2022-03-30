0c006af76abc4e77a8d162f0d539130f460b9f4c
josusk8 <josusk8@gmail.com>
2022-03-30 14:02:51 +0100

    Test IGIC or IVA . ok

--- src/CsvFilter.java
+++ src/CsvFilter.java
@@ -10,30 +10,39 @@ public class CsvFilter {
             "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, Cif_Cliente, NIF_Cliente \r\n" +
                     "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134, \r\n" +
                     "2,03/08/2019,2000,2000,,8,MacBook Pro,,78544372A\r\n" +
-                    "3,03/12/2019,1000,2000,19,9,Lenovo Laptop,,78544372A";
+                    "3,03/12/2019,1000,2000,19,9,Lenovo Laptop,,78544372A\r\n" +
+                    "4,02/08/2019,5000,2000,,8,MacBook Pro Plus,CIA99893,78544372A";
 
 
     static int dividerJumps() {
         lines = entry.split("\\r\\n");
         return lines.length;
     }
-
-    static ArrayList<String[]> getArrayList() {
-        dividerJumps();
+    static ArrayList<String[]> splitLines(){
 
         for (String line : lines) {
             String[] split = line.split(",");
             arrayListSplit.add(split);
         }
+        return arrayListSplit;
+    }
 
-    //Saltamos la cabecera y limpiamos
-        for (int i = 1; i < arrayListSplit.size()-1; i++) {
+    static ArrayList<String[]> splitIgicIva(){
+
+        for (int i =1 ; i < arrayListSplit.size()-1 ; i++) {
             String[] line = arrayListSplit.get(i);
-            if (line[4] != "" && line[4]!= "") {
+            if (!"" .equals(line[4]) && !"" .equals(line[5])) {
                 arrayListSplit.remove(i);
             }
         }
+        return arrayListSplit;
+    }
 
+
+    static ArrayList<String[]> getArrayList() {
+        dividerJumps();
+        splitLines();
+        splitIgicIva();
 
         return arrayListSplit;
 
--- src/CsvFilterShould.java
+++ src/CsvFilterShould.java
@@ -3,22 +3,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CsvFilterShould {
-    int lines_num = 4;
+    int lines_num = 5;
     int colum_num = 9;
     String entry =
             "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, Cif_Cliente, NIF_Cliente \r\n" +
-                    "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,\r\n" +
-                    "2,03/08/2019,2000,2000,,8,MacBook Pro,,78544372A\r\n" +
-                    "3,03/12/2019,1000,2000,19,9,Lenovo Laptop,,78544372A";
+                    "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134, \r\n" +
+                    "2,03/08/2019,2000,2000,,8,MacBook Pro,,78544372A \r\n" +
+                    "3,03/12/2019,1000,2000,19,9,Lenovo Laptop,,78544372A \r\n" +
+                    "4,02/08/2019,5000,2000,,8,MacBook Pro Plus,CIA99893,78544372A";
 
     @Test
     void num_jumps_lines() {
         assertEquals(lines_num, CsvFilter.dividerJumps());
-    }
-
-    @Test
-    void get_array_whith_size_of_jumps_lines() {
-        assertEquals(lines_num, CsvFilter.getArrayList().size());
     }
 
     @Test
@@ -32,11 +28,16 @@ public class CsvFilterShould {
     @Test
     void exclude_lines_whith_igic_and_iva() {
 
-        assertEquals(3, CsvFilter.getArrayList().size());
+        assertEquals(4, CsvFilter.getArrayList().size());
 
     }
 
+    @Test
+    void exclude_lines_whith_nif_and_cif() {
 
+        assertEquals(3, CsvFilter.getArrayList().size());
+
+    }
 
 
 }
